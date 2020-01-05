9d967a55b9346ad4d2991afb6716e35f5279a751
Umherirrender <umherirrender_de.wp@web.de>
2020-01-05 12:53:03 +0100

    build: Updating composer dependencies
    composer:
    * jakub-onderka/php-console-highlighter: 0.3.2 -> 0.4.0
    * mediawiki/mediawiki-phan-config: 0.4.0 -> 0.9.0
    
    Change-Id: I21c46864ff0f1c51ce50ceae3ad0c116c268c489

--- src/Alea.php
+++ src/Alea.php
@@ -83,6 +83,7 @@ class Alea {
 	 */
 	public function fract53(): float {
 		return $this->random() +
+			// @phan-suppress-next-line PhanTypeInvalidBitwiseBinaryOperator
 			( $this->random() * 0x200000 | 0 ) * 1.1102230246251565e-16; // 2^-53
 	}
 
